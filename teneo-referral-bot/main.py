27e8a92c7feff8a82d3cd1e93f00d1e4c7c99796
dwisn1922 <dwisn1922@gmail.com>
2025-04-23 01:38:48 +0700

    Update main.py

--- teneo-referral-bot/main.py
+++ teneo-referral-bot/main.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-# Tambahkan path root project
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Tambahkan path root ke Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
 from core.bot import TeneoReferralBot
 
