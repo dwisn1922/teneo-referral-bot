3f8be4a3ee7027fa8c8d242c2a3e9fed12331546
dwisn1922 <dwisn1922@gmail.com>
2025-04-23 01:39:08 +0700

    Update run.py

--- teneo-referral-bot/run.py
+++ teneo-referral-bot/run.py
@@ -1,4 +1,11 @@
-from teneo_referral_bot.core.bot import TeneoReferralBot
+import sys
+import os
+
+# Pastikan path benar
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, current_dir)
+
+from core.bot import TeneoReferralBot
 
 if __name__ == "__main__":
     print("Starting bot...")
